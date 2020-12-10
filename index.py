6291b62172211e3834641f1c76af2a1967c720e6
forbelieveme <hanetrr@gmail.com>
2020-12-09 22:14:55 -0500

    Genera metodo para verificacion de los intervalos de cada una de las letras

--- index.py
+++ index.py
@@ -3,16 +3,72 @@ import pandas as pd
 
 
 df = pd.read_csv('cartonescomas2.csv')
-# print(df.head())
-print(len(df))
-print(len(df.columns))
-b = df.loc[:,['n1b1','n1b2','n1b3','n1b4','n1b5']]
 
-print(len(b))
-print(len(b.columns))
 
-print(b[b['n1b1'] > 15])
+def verificarIntervalos(data):
+    nombre = {'campos': [
+        {'col': [
+            'n1b1',
+            'n1b2',
+            'n1b3',
+            'n1b4',
+            'n1b5',
+        ],
+            'low':14,
+            'high':16},
+        {'col': [
+            'n1i1',
+            'n1i2',
+            'n1i3',
+            'n1i4',
+            'n1i5',
+        ],
+            'low':29,
+            'high':31},
+        {'col': [
+            'n1n1',
+            'n1n2',
+            'n1n3',
+            'n1n4',
+            'n1n5',
+        ],
+            'low':30,
+            'high':46},
+        {'col': [
+            'n1g1',
+            'n1g2',
+            'n1g3',
+            'n1g4',
+            'n1g5',
+        ],
+            'low':45,
+            'high':61},
+        {'col': [
+            'n1o1',
+            'n1o2',
+            'n1o3',
+            'n1o4',
+            'n1o5',
+        ],
+            'low':60,
+            'high':76},
 
-# file = open("cartonescomas.csv")
-# numline = len(file.readlines())
-# print (numline)
+    ],
+        'letra': ['b', 'i', 'n', 'g', 'o']
+    }
+
+    for counti, i in enumerate(nombre['campos']):
+        nombre['info'+nombre['letra'][counti]] = data.loc[:, i['col']]
+
+    for counti, i in enumerate(nombre['campos']):
+        for countj, j in enumerate(i['col']):
+            letra = nombre['letra'][counti]
+            base = nombre['info'+letra]
+            temp = base[(base[j] < i['low']) | (base[j] > i['high'])]
+            print(temp)
+
+
+def main():
+    verificarIntervalos(df)
+
+main()
