189b0fac39642eb2f9d901f8a66a5b37ab9d6ea5
forbelieveme <hanetrr@gmail.com>
2020-12-09 19:52:21 -0500

    Crea una comparacion booleana para verificar los datos de validacion numerica

--- index.py
+++ index.py
@@ -1,8 +1,17 @@
+import numpy as np
 import pandas as pd
+
+
 df = pd.read_csv('cartonescomas2.csv')
 # print(df.head())
 print(len(df))
 print(len(df.columns))
+b = df.loc[:,['n1b1','n1b2','n1b3','n1b4','n1b5']]
+
+print(len(b))
+print(len(b.columns))
+
+print(b[b['n1b1'] > 15])
 
 # file = open("cartonescomas.csv")
 # numline = len(file.readlines())
