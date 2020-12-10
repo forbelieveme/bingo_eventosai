a41b874abc30f62eed97eea6e9e225c8752c7ca1
forbelieveme <hanetrr@gmail.com>
2020-12-09 19:25:14 -0500

    Usa la librería panda para trabajar el archivo csv con mayor facilidad

--- index.py
+++ index.py
@@ -1,4 +1,9 @@
+import pandas as pd
+df = pd.read_csv('cartonescomas2.csv')
+# print(df.head())
+print(len(df))
+print(len(df.columns))
 
-file = open("cartonescomas.csv")
-numline = len(file.readlines())
-print (numline)
+# file = open("cartonescomas.csv")
+# numline = len(file.readlines())
+# print (numline)
